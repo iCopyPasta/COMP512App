fff54b0a841ee5229529e253cbbfadf6ad81569a
peo5032 <thepabloski@gmail.com>
2018-02-12 14:45:56 -0500

    disable auto correct and keyboard hover for landscape

--- app/src/main/java/com/example/pabloandtyler/comp512app/MainActivity.java
+++ app/src/main/java/com/example/pabloandtyler/comp512app/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.pabloandtyler.comp512app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText wordSpace = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText wordSpace = (EditText) findViewById(R.id.type_space);
+        wordSpace.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+
+
     }
 }
